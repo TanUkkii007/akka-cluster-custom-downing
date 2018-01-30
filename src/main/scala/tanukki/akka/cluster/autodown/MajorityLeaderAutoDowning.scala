e6b568d39fe4224af633034267de0506dfead373
Matthew Borg <matthew.borg@ixaris.com>
2018-01-30 09:28:43 +0100

    Added the down removal margin as a configuration

--- src/main/scala/tanukki/akka/cluster/autodown/MajorityLeaderAutoDowning.scala
+++ src/main/scala/tanukki/akka/cluster/autodown/MajorityLeaderAutoDowning.scala
@@ -2,6 +2,7 @@ package tanukki.akka.cluster.autodown
 
 import akka.actor.{ActorSystem, Address, Props}
 import akka.cluster.{Cluster, DowningProvider}
+import com.typesafe.config.Config
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -10,16 +11,24 @@ class MajorityLeaderAutoDowning(system: ActorSystem) extends DowningProvider {
 
   private[this] val cluster = Cluster(system)
 
-  override def downRemovalMargin: FiniteDuration = cluster.downingProvider.downRemovalMargin
+  private val config: Config = system.settings.config
+
+  override def downRemovalMargin: FiniteDuration = {
+    val key = "custom-downing.down-removal-margin"
+    config.getString(key) match {
+      case "off" ⇒ Duration.Zero
+      case _     ⇒ Duration(config.getDuration(key, MILLISECONDS), MILLISECONDS)
+    }
+  }
 
   override def downingActorProps: Option[Props] = {
-    val stableAfter = system.settings.config.getDuration("custom-downing.stable-after").toMillis millis
+    val stableAfter = config.getDuration("custom-downing.stable-after").toMillis millis
     val majorityMemberRole = {
-      val r = system.settings.config.getString("custom-downing.majority-leader-auto-downing.majority-member-role")
+      val r = config.getString("custom-downing.majority-leader-auto-downing.majority-member-role")
       if (r.isEmpty) None else Some(r)
     }
-    val downIfInMinority = system.settings.config.getBoolean("custom-downing.majority-leader-auto-downing.down-if-in-minority")
-    val shutdownActorSystem = system.settings.config.getBoolean("custom-downing.majority-leader-auto-downing.shutdown-actor-system-on-resolution")
+    val downIfInMinority = config.getBoolean("custom-downing.majority-leader-auto-downing.down-if-in-minority")
+    val shutdownActorSystem = config.getBoolean("custom-downing.majority-leader-auto-downing.shutdown-actor-system-on-resolution")
     Some(MajorityLeaderAutoDown.props(majorityMemberRole, downIfInMinority, shutdownActorSystem, stableAfter))
   }
 }
